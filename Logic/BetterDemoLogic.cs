76510f73953b87b69f52b8e39b14c1a771621bd9
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-07-25 19:59:50 +0300

    first commit

---
+++ Logic/BetterDemoLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DependencyInjectionDemo.Logic
+{
+	public class BetterDemoLogic : IDemoLogic
+	{
+		public int Value1 { get; private set; }
+		public int Value2 { get; private set; }
+
+		public BetterDemoLogic()
+		{
+			Value1 = 25;
+			Value2 = 50;
+		}
+	}
+
+}
